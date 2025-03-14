d66c6fdb78bac0cffcd4f71662aa402840495d2f
wenjunxp@sina.com <wenjunxp@sina.com>
2025-03-14 11:24:37 +0800

    first commit

---
+++ src/main/java/com/atguigu/cloud/flowabledemo2025/FlowableDemo2025Application.java
@@ -0,0 +1,13 @@
+package com.atguigu.cloud.flowabledemo2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowableDemo2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowableDemo2025Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/atguigu/cloud/flowabledemo2025/FlowableDemo2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.cloud.flowabledemo2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowableDemo2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
