499b584666548e2621c1ceabe9e9c20177e7e423
wenjunxp@sina.com <wenjunxp@sina.com>
2025-03-19 13:29:47 +0800

    test run task in springboot

--- src/test/java/com/atguigu/cloud/flowabledemo2025/FlowableDemo2025ApplicationTests.java
+++ src/test/java/com/atguigu/cloud/flowabledemo2025/FlowableDemo2025ApplicationTests.java
@@ -1,15 +1,20 @@
 package com.atguigu.cloud.flowabledemo2025;
 
+import lombok.extern.slf4j.Slf4j;
 import org.flowable.engine.*;
 import org.flowable.engine.repository.Deployment;
+import org.flowable.task.api.Task;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashMap;
 
 /***
  * 第一个测试
  */
 @SpringBootTest
+@Slf4j
 class FlowableDemo2025ApplicationTests {
     @Autowired
     ProcessEngine  processEngine;
@@ -31,6 +36,30 @@ class FlowableDemo2025ApplicationTests {
 
         System.out.println("qingjia.getId() = " + qingjia.getId());
         System.out.println("qingjia.getName() = " + qingjia.getName());
+    }
+    @Test
+    void startProcess() {
+        HashMap<String, Object> variables = new HashMap<>();
+        variables.put("assignee0","zhangsan");
+        variables.put("assignee1","lisi");
+        runtimeService.startProcessInstanceByKey("SpringBootTest000Key",variables);
+
+    }
+    @Test
+    void testZhangsanCompleteTask() {
+        Task task = taskService.createTaskQuery().processDefinitionKey("SpringBootTest000Key").taskAssignee("zhangsan").singleResult();
+        log.info("task.getId() = " + task.getId());
+        log.info("task.getName() = " + task.getName());
+        log.info("task.getAssignee() = " + task.getAssignee());
+        taskService.complete(task.getId());
+    }
+    @Test
+    void testLisiCompleteTask() {
+        Task task = taskService.createTaskQuery().processDefinitionKey("SpringBootTest000Key").taskAssignee("lisi").singleResult();
+        log.info("task.getId() = " + task.getId());
+        log.info("task.getName() = " + task.getName());
+        log.info("task.getAssignee() = " + task.getAssignee());
+        taskService.complete(task.getId());
     }
 
 }
